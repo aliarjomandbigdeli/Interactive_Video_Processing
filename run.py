c23a031d8832133d7e75cae0253d34c780f23691
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-01-21 19:56:03 +0330

    initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+import cv2
+
+"""
+.. sectionauthor:: Ali ArjomandBigdeli  <https://github.com/aliarjomandbigdeli>
+.. since:: 1/21/2019
+"""
+
+def main():
+    print("hello")
+
+if __name__ == '__main__': main()
